04b839ae5504b99116364815e5c1bf603bcc3c40
SandhuNarinder <57005315+SandhuNarinder@users.noreply.github.com>
2020-04-23 21:32:07 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,124 @@
+<!DOCTYPE HTML>
+<html lang="en">
+
+<head>
+    <title>Homepage - Narinder Singh Sandhu</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="keywords" content=" website designer, web developer, website creator,  web page designer, responsive web developer" />
+    <meta name="description" content="Narinder Sandhu - Web Developer and Web Designer " />
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+    <div id="wrapper">
+     
+
+        <div id="top">
+            <header>
+                <a href="#"><img id="logo" src="images/logo.png" alt="site logo">
+                <h2>Narinder Sandhu</h2></a>
+                
+            </header>
+            <nav>
+                <ul class="navbar">
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="About.html">About</a></li>
+                    <li><a href="work.html">My Work</a></li>
+                    <li><a href="contact.html">Contact</a></li>
+                </ul>
+            </nav>
+        </div>
+
+        <main>
+            <div class="main_slider">
+
+                <div class="slider">
+                    <img class="front_image" src="images/home.jpg" alt="slider_img1">
+                </div>
+
+            </div>
+                  <div id="service">
+                    <div class="heading">
+                        <h2>My Links</h2>
+
+                    </div>
+
+                <div class="services-container">
+
+                    <div class="service1">
+                        <img  src="images/web%20design.png" alt="web design" class="link_img">
+                        <h2>Web Developer</h2>
+                        <a class="btn" href="resume/Narinder_resume.docx" download>Download my CV</a>
+
+                    </div>
+
+
+                    <div class="service1">
+                        <img src="images/storage.png" alt="site" class="link_img">
+                        <h2>My Site Link</h2>
+                        <a class="btn" href="http://magicapparels.altervista.org/wordpress/" target="_blank">My Website</a>
+
+                    </div>
+                </div>
+
+            </div>
+                 
+        </main>
+       <footer class="main_footer">
+            <div class="left_footer">
+                <h3><span id="name">Narinder Sandhu</span></h3>
+<!--
+                <p class="footer-links">
+                    <a href="index.html"><u>Home</u></a>
+                    <a href="About.html"><u>About</u></a>
+                    <a href="courses.html"><u>Courses</u></a>
+                    <a href="teachers.html"><u>Teachers</u></a>
+                    <a href="Pricing.html"><u>Pricing</u></a>
+                    <a href="Contact.html"><u>Contact</u></a>
+                </p>
+-->
+                <p class="footer_company">Portfolio &copy; 2020 Narinder Sandhu</p>
+            </div>
+<!--
+            <div class="footer_center">
+                <div>
+                    <p>Address:</p>
+                    <p>Homer Watson, Kitchener, ON</p>
+                </div>
+
+                <div>
+                    <p>Phone Number:</p>
+                    <p>+1 000 123456</p>
+                </div>
+                <div>
+                    <p>Email:</p>
+                    <p id="mail"><a href="mailto:narindersandhu1998@gmail.com.com">Kiddos@company.com</a></p>
+                </div>
+            </div>
+-->
+            <div class="right_footer">
+
+<!--
+                <p class="footer_about">
+                    <span>About the company</span>
+                    Educational Website.
+                </p>
+-->
+                
+             <p>Social Media:</p>
+                <a href="https://www.facebook.com/narinder.sandhu.39" target="_blank"><i class="fa fa-facebook"></i></a>
+                <a href="mailto:narindersandhu1998@gmail.com.com" target="_blank"><i class="fa fa-envelope"></i></a>
+                <a href="https://www.youtube.com/" target="_blank"><i class="fa fa-youtube"></i></a>
+                <a href="https://www.instagram.com/narinder_sandhu07/" target="_blank"><i class="fa fa-instagram"></i></a>
+                <a href="https://www.linkedin.com/in/narinder-sandhu-490b03196/" target="_blank"><i class="fa fa-linkedin"></i></a>
+            </div>
+        </footer>
+
+    </div>
+</body>
+
+</html>
+
+    
